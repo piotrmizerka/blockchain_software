16b11182ac39fde0c7a732d0cbd4c19c25712742
piotrmizerka <piotrmizerka@gmail.com>
2021-01-08 16:03:37 +0100

    Deleted everything as desired to rebuild from scratch and in order to make it clearer to track the changes.

--- bitcoin_analysis/auxiliaryRenderingFunctions.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void drawText(const char *text, int length, double x, double y, double r, double g, double b);
-void changeViewPort(int w, int h);
-void drawArc(float cx, float cy, float r, float start_angle, float arc_angle, int num_segments);
