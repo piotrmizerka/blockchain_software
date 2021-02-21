3cd1cbdf5672a5b5fd98a864ff2874639173f0b2
piotrmizerka <piotrmizerka@gmail.com>
2021-02-21 10:19:24 +0100

    Description of the sort script usage.

--- sortTx.sh
+++ sortTx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# In order to sort txin.dat and txout.dat by txidies, first give the necessary permissions to sortTx.sh with the command "chmod +x sortTx.sh" and then run "./sortTx.sh path_to_txin.dat path_to_txout.dat" (assuming you are in the same directory as the sortTx.sh script file).
+
 # Display messages if txin.dat or txout.dat is not sorted
 sort -c -n -k 1 -k 2 -k 3 -k 4 -k 5 -k 6 $1
 sort -c -n -k 1 -k 2 -k 3 -k 4 -k 5 -k 6 $2
