827e3382b9d3494dae7ad964afa2baab9e9dcd19
piotrmizerka <piotrmizerka@gmail.com>
2021-02-27 18:17:13 +0100

    This is supposed to be the main script creating the whole users graph with auxiliary data.

---
+++ usersGraph.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Sources used for this script: 
+# (1) https://senseable2015-6.mit.edu/bitcoin/
+# (2) https://github.com/dkondor/txedges
+# (3) https://github.com/dkondor/join-utils
+
+
