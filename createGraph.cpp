0ae0fb73315439856e7e2dd6b48d6b12c92a6ba3
piotrmizerka <piotrmizerka@gmail.com>
2021-05-06 16:17:19 +0200

    Users graph creation implemented in C++ to make it much faster

---
+++ createGraph.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    // Determine maximal bitcoin address to know how big the vector to store user idies for bitcoin addresses need to be
+    int maxAddress = 0;
+    FILE *readContractedAddresses;
+    int bitcoinAddress, userIdx;
+    while(!feof(readContractedAddresses))
+    {
+        fscanf(readContractedAddresses,"%i %i",&bitcoinAddress,&userIdx);
+        if(maxAddress<bitcoinAddress)maxAddress = bitcoinAddress;
+    }
+    fclose(readContractedAddresses);
+
+    // Save user idies for Bitcoin addresses in the memory - in the vector
+    vector<int> userId(maxAddress+1);
+    readContractedAddresses = fopen(argv[1], "r");
+    while(!feof(readContractedAddresses))
+    {
+        fscanf(readContractedAddresses,"%i %i",&bitcoinAddress,&userIdx);
+        userId[bitcoinAddress] = userIdx;
+    }
+    fclose(readContractedAddresses);
+
+    // Save users graph in the dedicated file - usersGraph.dat
+    FILE *readTxEdgesTimes, *saveUsersGraph;
+    readTxEdgesTimes = fopen(argv[2],"r");
+    saveUsersGraph = fopen(argv[3],"w");
+    int inputBitcoinAddress, outputBitcoinAddress, timeStamp;
+    float bitcoinAmount;
+    while (!feof(readTxEdgesTimes))
+    {
+        fscanf(readTxEdgesTimes,"%i %i %f %i", &inputBitcoinAddress, &outputBitcoinAddress, &bitcoinAmount, &timeStamp);
+        fprintf(saveUsersGraph, "%i %i %.0lf %i\n",userId[inputBitcoinAddress],userId[outputBitcoinAddress],bitcoinAmount,timeStamp);
+    }
+    fclose(readTxEdgesTimes);
+    fclose(saveUsersGraph);
+
+    return 0;
+}
