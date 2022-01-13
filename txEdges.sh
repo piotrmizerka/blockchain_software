fb7b575e8b0e72073f9215dbffbf19a6d385989c
piotrmizerka <piotrmizerka@gmail.com>
2022-01-13 13:52:41 +0100

    Descriptions corrected

--- txEdges.sh
+++ txEdges.sh
@@ -12,7 +12,10 @@
 #
 # The script requires the following 3 path parameters (appearing as parameters in that order):
 #
-# (1) txinPath - the path to the txin.dat file (not compressed) as specified at https://github.com/dkondor/bitcoin, Usage (dump mode),
+# (1) txinPath - the path to the txin.dat file (not compressed) as specified at https://github.com/dkondor/bitcoin, Usage (dump mode)
+#              - altghough the specification of txin.dat may not reflect the final format of txin.dat
+#              - I noticed this files contains rows consisting of 6 numbers instead of 7 - what is true
+#                (and also important for us) is that the first number is "txId" and the last two are "addrID" and "sum",
 # (2) txoutPath - the path to the txout.dat file (not compressed) as specified at as specified at https://github.com/dkondor/bitcoin, Usage (dump mode),
 # (3) txedgesPath - the path to the output file (whose form has been specified at the beginning - lines 6 - 11),
 #
