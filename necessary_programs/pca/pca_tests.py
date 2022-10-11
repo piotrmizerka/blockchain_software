5a88744ab027f2345804999dc8ece517cd506443
piotrmizerka <piotrmizerka@gmail.com>
2022-10-11 15:29:30 +0200

    test dataMatrix
    small test randomized hopefully more human-readible

--- necessary_programs/pca/pca_tests.py
+++ necessary_programs/pca/pca_tests.py
@@ -9,6 +9,9 @@ import shutil
 from random import seed
 from random import randint
 import random
+from statistics import mean
+import numpy as np
+
 # TODO
 class Test_DataMatrix(unittest.TestCase):
     
@@ -24,34 +27,9 @@ class Test_DataMatrix(unittest.TestCase):
                 data_file.write(str(j)+" "+str(j)+" "+str(test_data[i][j])+"\n")
             data_file.close()
 
-    def testHumanReadible(self):
-        directory = "pca_test_data"
-        parent_dir = "./tst"
-        snapshots_dir_path = os.path.join(parent_dir, directory) 
-
-        test_data = [[1, 0, 0, 0],
-                     [1, 3, 0, 0],
-                     [1, 2, 4, 1],
-                     [0, 0, 0, 0]]
-        
-        self.saveTestData(test_data, snapshots_dir_path)
-         
-        original_data, transformed_data = dataMatrix(snapshots_dir_path)
-
-        self.assertEqual(original_data, [[1.0, 0.0, 0.0, 0.0],
-                                         [1.0, 3.0, 0.0, 0.0],
-                                         [1.0, 2.0, 4.0, 1.0],
-                                         [0.0, 0.0, 0.0, 0.0]])
-        self.assertEqual(transformed_data, [[1-(1+0.25+0.125)/3, -(0.75+0.25)/3, -0.5/3, -0.125/3],
-                                            [0.25-(1+0.25+0.125)/3, 0.75-(0.75+0.25)/3, -0.5/3, -0.125/3],
-                                            [0.125-(1+0.25+0.125)/3, 0.25-(0.75+0.25)/3, 0.5-0.5/3, 0.125-0.125/3],
-                                            [0.0, 0.0, 0.0, 0.0]])
-
-        shutil.rmtree(snapshots_dir_path)
-
     # Don't check transformed data - checks only reading from file actually.
     # Otherwise, we would have to repeat the code of the tested function.
-    def testRandomized(self): 
+    def test_dataMatrix_loading(self): 
         directory = "pca_test_data"
         parent_dir = "./tst"
         snapshots_dir_path = os.path.join(parent_dir, directory) 
@@ -76,7 +54,34 @@ class Test_DataMatrix(unittest.TestCase):
 
         self.assertEqual(original_data, [list(map(float,l)) for l in test_data])
 
-        # shutil.rmtree(snapshots_dir_path)
+        shutil.rmtree(snapshots_dir_path)
+    
+    def test_dataMatrix(self):
+        directory = "pca_test_data"
+        parent_dir = "./tst"
+        snapshots_dir_path = os.path.join(parent_dir, directory) 
+
+        snapshots_number = 4
+        features_number = 4
+        seed(randint(0,10))
+        test_data = [[0]*features_number]*snapshots_number
+        for i in range(0, snapshots_number-1): # the last row is zero
+            test_data[i] = [randint(1, 100) for k in range(0, features_number)]
+
+        self.saveTestData(test_data, snapshots_dir_path)
+         
+        original_data, transformed_data = dataMatrix(snapshots_dir_path)
+
+        self.assertEqual(original_data, test_data)
+        ar = average_rows = [[test_data[i][j]/sum(test_data[i]) for j in range(0,4)] for i in range(0,3)]
+        ac = average_columns = [mean([average_rows[i][j] for i in range(0,3)]) for j in range(0,4)]
+        proper_transformed_data = [[ar[0][0]-ac[0], ar[0][1]-ac[1], ar[0][2]-ac[2], ar[0][3]-ac[3]],
+                                   [ar[1][0]-ac[0], ar[1][1]-ac[1], ar[1][2]-ac[2], ar[1][3]-ac[3]],
+                                   [ar[2][0]-ac[0], ar[2][1]-ac[1], ar[2][2]-ac[2], ar[2][3]-ac[3]],
+                                   [0.0, 0.0, 0.0, 0.0]]
+        self.assertEqual(np.round(transformed_data,2).all(), np.round(proper_transformed_data,2).all())
+        
+        shutil.rmtree(snapshots_dir_path)
 
 class Test_pca_sklearn(unittest.TestCase):
 
